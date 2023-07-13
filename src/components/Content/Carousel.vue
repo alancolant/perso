aca436c5f49ea648be312eebae51d4a3d9e9a58b
Colant Alan <alan.colant@outlook.fr>
2023-07-13 19:28:15 +0200

    switch to embla

--- src/components/Content/Carousel.vue
+++ src/components/Content/Carousel.vue
@@ -1,92 +1,80 @@
 <script setup lang="ts">
-import {onBeforeUnmount, onMounted, ref, watch} from "vue";
+import {onBeforeUnmount, onMounted, ref, watch, watchEffect} from "vue";
+import emblaCarouselVue from 'embla-carousel-vue'
 
 interface Props {
   images: Array<{ src: string, alt: string | undefined }>
+  slidesPerView: number
 }
 
-const props = defineProps<Props>()
-
-const imageElements = ref<HTMLImageElement[]>([])
-const currentImagePosition = ref(0)
-
-const goToPrev = () => {
-  imageElements.value.at(currentImagePosition.value - 1)?.scrollIntoView();
-  currentImagePosition.value -= 1;
-}
-const goToNext = () => {
-  imageElements.value.at(currentImagePosition.value + 1)?.scrollIntoView();
-  currentImagePosition.value += 1;
-}
-
-onMounted(() => {
-  console.log('mounted')
-
-  const observer = new IntersectionObserver((entries) => entries.forEach((entry) => {
-    if (!entry.isIntersecting) return
-    currentImagePosition.value = imageElements.value.findIndex((image) => image === entry.target)
-  }))
-  imageElements.value.forEach((image) => observer.observe(image))
-  onBeforeUnmount(observer.disconnect)
+const props = withDefaults(defineProps<Props>(), {
+  slidesPerView: 1
 })
 
+const [emblaNode, emblaApi] = emblaCarouselVue({slidesToScroll: 1})
+const selectedScrollSnap = ref(0);
+
+watchEffect(() => {
+  if (!emblaApi.value) return
+  emblaApi.value?.on('select', (e) => {
+    selectedScrollSnap.value = e.selectedScrollSnap()
+  })
+})
+const scrollPrev = () => emblaApi.value?.scrollPrev();
+const scrollNext = () => emblaApi.value?.scrollNext();
+const scrollTo = (index: number) => emblaApi.value?.scrollTo(index);
 </script>
 <template>
-  <div class="galery-container" ref="galleryContainer">
-    <span class="prev" @click="goToPrev" v-if="currentImagePosition>0">&lt;</span>
-    <div class="galery">
-      <img :id="`image-${index}`"
-           v-for="(image,index) in images"
-           :src="image.src"
-           :alt="image.alt || `Image ${index}`"
-           ref="imageElements"/>
-    </div>
-    <span class="next" @click="goToNext" v-if="currentImagePosition<images?.length-1">&gt;</span>
-  </div>
+  <div class="embla">
+    <div class="embla__viewport" ref="emblaNode">
+      <div class="embla__container" :style="`--slide-size:${100/slidesPerView}%`">
+        <div class="embla__slide" v-for="(image,index) in images">
+          <img :src="image.src" :alt="image.alt || `Image ${index}`"/>
+        </div>
 
+      </div>
+    </div>
+    <!--NEXT/PREV-->
+    <button class="prev" v-if="selectedScrollSnap>0" @click="scrollPrev()">&lt;</button>
+    <button class="next" v-if="selectedScrollSnap<images.length-1" @click="scrollNext()">&gt;</button>
+
+  </div>
 </template>
 
 <style scoped>
-
-.galery-container {
-  @apply aspect-[16/9] w-full relative;
+.embla {
+  overflow: hidden;
+  position: relative;
 }
 
-.galery {
-  @apply w-full h-full flex  gap-x-4;
-  overflow-x: auto;
-  scroll-behavior: smooth;
-  scroll-snap-type: x mandatory;
-  scroll-snap-stop: always;
-  -webkit-overflow-scrolling: touch;
+.embla__container {
+  /*@apply grid grid-flow-col grid-cols-1;*/
+  display: grid;
+  grid-auto-flow: column;
+  grid-auto-columns: var(--slide-size);
 }
 
+.embla__slide {
+  min-width: 0;
+}
 
-.galery-container :is(.prev,.next) {
-  @apply absolute block w-12 h-12 top-1/2 rounded-full bg-gray-500 cursor-pointer;
+.embla__slide img {
+  @apply w-full h-full object-cover;
+}
+
+.embla :is(.prev,.next) {
+  @apply absolute block w-12 h-12 top-1/2 rounded-full bg-gray-500 cursor-pointer -translate-y-1/2;
   font-size: 3rem;
   line-height: 3rem;
   text-align: center;
 }
 
-.galery-container .prev {
+.embla .prev {
   @apply ml-4 left-0;
 }
 
-.galery-container .next {
+.embla .next {
   @apply right-0 mr-4;
 }
 
-@media (hover) {
-  .galery {
-    overflow: hidden;
-  }
-}
-
-.galery img {
-  flex: 0 0 100%;
-  display: block;
-  scroll-snap-align: center;
-  height: auto;
-}
 </style>
