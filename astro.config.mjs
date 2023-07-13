f907f1845fcb5283a1f49b74429303b91f690331
Colant Alan <alan.colant@outlook.fr>
2023-07-13 03:39:42 +0200

    f

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,10 @@ import {defineConfig} from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 
 import prefetch from "@astrojs/prefetch";
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://www.alancolant.com",
-    output: 'hybrid',
-    adapter: node({
-        mode: 'standalone',
-    }),
+    base: "/perso",
     integrations: [sitemap({}), prefetch()]
 });
