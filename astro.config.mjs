1bce0f12dd1c110f4830dd07bc9e969fe12d4b05
Colant Alan <alan.colant@outlook.fr>
2023-07-14 14:26:33 +0200

    publish

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://dev.alancolant.com",
+  site: "https://www.alancolant.com",
   integrations: [sitemap({}), prefetch(),
   tailwind(),
   mdx({
